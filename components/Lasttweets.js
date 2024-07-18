148b16603134ed7246f2b78f13a48691fa8b6664
Mathieu <m.hembert@outlook.fr>
2024-07-18 20:36:13 +0200

    modification Lasttweets

--- components/Lasttweets.js
+++ components/Lasttweets.js
@@ -1,13 +1,24 @@
 import styles from '../styles/Lasttweets.module.css'
 import Tweet from './Tweet';
+import { useState, useEffect } from 'react'
 
 function Lasttweets() {
+  const urlBackEnd = 'http://localhost:3000';
+  const [dataTweets,setDataTweets]=useState([]);
+
+  useEffect(() => {
+    fetch(`${urlBackEnd}/tweets}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.result){
+          setDataTweets(data.tweets);
+        }
+      });
+
+  }, [dataTweets])
 
   return (
     <div>
-      <p style={{color:'white'}}>test</p>
-      <p style={{color:'white'}}>test</p>
-      <p style={{color:'white'}}>test</p>
       <Tweet firstname="test"/>
     </div>
   );
