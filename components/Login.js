6d73aeda5934ee60fecdc3eb30f12095f11fe6d0
Renaud69100 <r.akehal@gmail.com>
2024-07-18 18:21:30 +0200

    logos

--- components/Login.js
+++ components/Login.js
@@ -1,30 +1,36 @@
 import styles from '../styles/Home.module.css';
-//importer images depuis dossier public
+import logo from 'twitterLogo.png'
 
 function Login() {
 
 
-    function handlesignup() {}
+    function handlesignup() {
+        //show signup
+    }
 
-    function handlesignin() {}
+    function handlesignin() {
+        //show signin
+    }
 
-    
   return (
     <div>
-        <pageContainer className={styles.main} >
+        <div className={styles.main} >
             <div className={styles.imgContainer}>
-                <img src='twitterBackground' className={styles.loginBackground}></img>
-                <img src='twitterLogo' className={styles.twitterIcon}></img>
+                <img alt="background image" src='twitterBackground' className={styles.loginBackground}></img>
+                <img src={twitterLogo.png} alt="Main logo" className={styles.twitterIcon} />
             </div>  
             <div className={styles.loginContainer}>
-                <img src='twitterLogo' className={styles.twitterIcon}></img>
+                <img src='logo' className={styles.twitterIcon}></img>
                 <h1 className={styles.title}>See what's happening</h1>
                 <h2 className={styles.underTitle}>Join hacketweet today.</h2>
+                <input type="text" placeholder="name" id="username" onChange={(e) => setSignUpSignupFirstname(e.target.value)} value={SignupFirstname} />
+                <input type="text" placeholder="name" id="firstname" onChange={(e) => setSignUpUsername(e.target.value)} value={SignUpUsername} />
+                <input type="password" placeholder="name" id="firstname" onChange={(e) => setSignupPassword(e.target.value)} value={SignupPassword} />
                 <button id="signup" onClick={() => handlesignup()}>Sign up</button>
                 <p className={styles.SigninInvit}>Already have an account ?</p>
-                <button id="signup" onClick={() => handlesignin()}>Sign up</button>
+                <button id="signup" onClick={() => handleLogin()}>Sign up</button>
             </div>
-        </pageContainer>
+        </div>
     </div>
   );
 }
--- components/Signup.js
+++ components/Signup.js
@@ -1,20 +1,19 @@
 import styles from '../styles/Signup.module.css';
 
 function Signup() {
-//logo
-//create your hackatweet account
-//name
-//username
-//password
-//btn : Sign up 
+
+    
 
 function setSignUpUsername() {}
+
 
     return = (
         <div className={styles.main}>
             <img src='twitterLogo' className={styles.twitterIcon}></img>
             <p className={styles.title}>create your hackatweet account</p>
-            <input type="text" placeholder="name" id="username" onChange={(e) => setSignUpUsername(e.target.value)} value={SignUpUsername} />
+            <input type="text" placeholder="name" id="username" onChange={(e) => setSignUpSignupFirstname(e.target.value)} value={SignupFirstname} />
+            <input type="text" placeholder="name" id="firstname" onChange={(e) => setSignUpUsername(e.target.value)} value={SignUpUsername} />
+            <input type="password" placeholder="name" id="firstname" onChange={(e) => setSignupPassword(e.target.value)} value={SignupPassword} />
         </div>
     )
 
